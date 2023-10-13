71cb9bb0227ee7ef6136521b2092c6deba29f531
Thomas Jindu <thomas@spectrumits.co.zw>
2023-10-13 08:10:01 +0200

    Added Testimonials css

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,9 +1,47 @@
-import './Testimonials.css'
+
+import { Pagination, Autoplay } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import testimonials from "./data";
+import Testimonial from "./Testimonial";
+import "./Testimonials.css";
 
 const Testimonials = () => {
   return (
-    <section id="testimonials"><h2>Testimonials</h2></section>
-  )
-}
+    <section id='testimonials'>
+      <h2>What My Clients Say</h2>
+      <p>These are unbiased testmonials from some of my clients</p>
+      <div className='container'>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            601: { slidesPerView: 2 },
+            1025: { slidesPerView: 3 },
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Autoplay]}
+          autoplay={true}
+          className='mySwiper'
+        >
+          {testimonials.map((testimonial) => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
