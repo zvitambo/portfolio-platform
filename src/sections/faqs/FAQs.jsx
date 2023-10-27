aa11c8b354697b34972e38dedef0fa24c0cea662
Thomas Jindu <thomas@spectrumits.co.zw>
2023-10-27 14:53:45 +0200

    Added a Faqs section

--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,9 +1,17 @@
 import './FAQs.css'
+import faqs from './data';
+import FAQ from './FAQ';
 
 const FAQs = () => {
   return (
-   <section id="faqs"><h2>FAQS</h2></section>
-  )
+    <section id='faqs'>
+      <h2>FAQS</h2>
+      <p></p>
+      <div className='container faqs__container'>{faqs.map(faq =>(
+        <FAQ key={faq.id} faq={faq}/>
+      ))}</div>
+    </section>
+  );
 }
 
 export default FAQs
