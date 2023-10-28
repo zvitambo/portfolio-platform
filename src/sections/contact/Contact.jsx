d15fec656312eb6cffe2490be65e811b08de4401
Thomas Jindu <thomas@spectrumits.co.zw>
2023-10-28 09:31:13 +0200

    Added the contacts section

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,11 +1,25 @@
-import './Contact.css'
+import contacts from "./data";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <section id='contact'>
-      <h2>Contact</h2>
+      <h2>Get In Touch</h2>
+      <p>Message me via any of the links below!</p>
+      <div className='container contact__container'>
+        {contacts.map((contact) => (
+          <a
+            key={contact.id}
+            href={contact.link}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {contact.icon}
+          </a>
+        ))}
+      </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
---
+++ src/sections/contact/data.js
@@ -0,0 +1,17 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:thomas.jindu2112@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/100092709343728'},
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+263773723049'}
+]
+
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+// https://api.whatsapp.com/send/?phone=%2B233557097546
+
+
+export default data
